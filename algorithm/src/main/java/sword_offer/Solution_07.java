ab73b64028406cf19d4d77d99432b6b126917b93
roy <lixianji85@sina.com>
2022-03-27 23:28:45 +0800

    update algorithm module

--- algorithm/src/main/java/sword_offer/Solution_07.java
+++ algorithm/src/main/java/sword_offer/Solution_07.java
@@ -1,6 +1,9 @@
 package sword_offer;
 
 
+import java.util.HashMap;
+import java.util.Map;
+
 //输入某二叉树的前序遍历和中序遍历的结果，请构建该二叉树并返回其根节点。
 //假设输入的前序遍历和中序遍历的结果中都不含重复的数字。
 public class Solution_07 {
@@ -10,10 +13,33 @@ public class Solution_07 {
        TreeNode right;
        TreeNode(int x) { val = x; }
    }
-    public TreeNode buildTree(int[] preorder, int[] inorder) {
-       // 复原node
+    private Map<Integer, Integer> indexMap;
 
-       return null;
+    private TreeNode buildTree(int[] preOrder, int preLeft, int preRight,
+                               int[] midOrder, int midLeft, int midRight) {
+
+        if (preLeft > preRight || midLeft > midRight) {
+            return null;
+        }
+        int rootValue = preOrder[preLeft];
+        TreeNode root = new TreeNode(rootValue);
+
+        int pIndex = indexMap.get(rootValue);
+
+        root.left = buildTree(preOrder, preLeft + 1, preLeft + pIndex - midLeft, midOrder, midLeft, pIndex - 1);
+        root.right = buildTree(preOrder, preLeft + pIndex - midLeft + 1, preRight, midOrder, pIndex + 1, midRight);
+
+        return root;
+    }
+
+    public TreeNode buildTree(int[] preorder, int[] inorder) {
+        int n = preorder.length;
+        // 构造哈希映射，帮助我们快速定位根节点
+        indexMap = new HashMap<Integer, Integer>();
+        for (int i = 0; i < n; i++) {
+            indexMap.put(inorder[i], i);
+        }
+        return buildTree(preorder, 0, n - 1,inorder, 0, n - 1);
     }
 }
 
---
+++ algorithm/src/main/java/sword_offer/Solution_09.java
@@ -0,0 +1,43 @@
+package sword_offer;
+
+
+import java.util.Stack;
+
+// 用两个栈实现一个队列。队列的声明如下，
+// 请实现它的两个函数 appendTail 和 deleteHead ，
+// 分别完成在队列尾部插入整数和在队列头部删除整数的功能。
+// (若队列中没有元素，deleteHead 操作返回 -1 )
+public class Solution_09 {
+    private static class CQueue {
+        private Stack<Integer> stack = new Stack<>();
+        private Stack<Integer> stackTemp = new Stack<>();
+
+        public CQueue() {
+        }
+
+        public void appendTail(int value) {
+            stack.push(value);
+        }
+
+        public int deleteHead() {
+            if (stack.empty()) {
+                if (stackTemp.empty()) {
+                    return -1;
+                }
+                return stackTemp.pop();
+            } else {
+                if (stackTemp.empty()) {
+                    in2out();
+                }
+                return stackTemp.pop();
+            }
+        }
+
+        private void in2out() {
+            while (!stack.empty()) {
+                stackTemp.push(stack.pop());
+            }
+        }
+
+    }
+}

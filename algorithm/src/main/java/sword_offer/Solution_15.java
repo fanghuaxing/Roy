c1f2acd55c3b08dcfecd22e1ccfbea8caacfbb66
roy <lixianji85@sina.com>
2022-03-30 23:12:27 +0800

    update algorithm module

---
+++ algorithm/src/main/java/sword_offer/Solution_15.java
@@ -0,0 +1,18 @@
+package sword_offer;
+
+
+// 答案需要取模 1e9+7（1000000007），如计算初始结果为：1000000008，请返回 1。
+public class Solution_15 {
+
+    public int hammingWeight(int n) {
+        String binaryString = Integer.toBinaryString(n);
+        int count = 0;
+        for (int i = 0; i < binaryString.length(); i++) {
+            char c = binaryString.charAt(i);
+            if (c == '1') {
+                count++;
+            }
+        }
+        return count;
+    }
+}

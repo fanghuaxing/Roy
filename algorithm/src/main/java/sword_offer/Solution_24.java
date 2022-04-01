c9e4cab052bec8b8acc22a3d9e86435ea048100b
roy <lixianji85@sina.com>
2022-04-01 22:50:24 +0800

    update algorithm module

---
+++ algorithm/src/main/java/sword_offer/Solution_24.java
@@ -0,0 +1,26 @@
+package sword_offer;
+
+public class Solution_24 {
+
+    public ListNode reverseList(ListNode head) {
+        ListNode cur = head, pre = null;
+        while(cur != null) {
+            ListNode tmp = cur.next; // 暂存后继节点 cur.next
+            cur.next = pre;          // 修改 next 引用指向
+            pre = cur;               // pre 暂存 cur
+            cur = tmp;               // cur 访问下一节点
+        }
+        return pre;
+    }
+    public class ListNode {
+        int val;
+        ListNode next;
+        ListNode(int x) {
+            val = x;
+        }
+    }
+}
+
+
+
+

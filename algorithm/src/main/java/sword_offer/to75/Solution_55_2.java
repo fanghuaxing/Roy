5a42da253565875b08a7d3dd0c47034035965026
roy <lixianji85@sina.com>
2022-04-05 22:53:58 +0800

    update algorithm module

--- algorithm/src/main/java/sword_offer/to75/Solution_55_2.java
+++ algorithm/src/main/java/sword_offer/to75/Solution_55_2.java
@@ -1,6 +1,22 @@
 package sword_offer.to75;
 
 
-public class Solution_55_2 {
+import sword_offer.TreeNode;
 
+public class Solution_55_2 {
+    public boolean isBalanced(TreeNode root) {
+        if (root == null) return true;
+        int i = maxDepth(root.left) - maxDepth(root.right);
+        boolean result = Math.abs(i) <= 1;
+        if (result) {
+            return isBalanced(root.left) && isBalanced(root.right);
+        } else {
+            return false;
+        }
+    }
+
+    public int maxDepth(TreeNode root) {
+        if (root == null) return 0;
+        return Math.max(maxDepth(root.left), maxDepth(root.right)) + 1;
+    }
 }

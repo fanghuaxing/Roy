df25c1fd596ca10f119ed2ea99a82b3af5b61798
roy <lixianji85@sina.com>
2022-04-11 23:21:39 +0800

    update algorithm module

---
+++ algorithm/src/main/java/sword_offer/to75/Solution_62.java
@@ -0,0 +1,16 @@
+package sword_offer.to75;
+
+
+public class Solution_62 {
+    public int lastRemaining(int n, int m) {
+        return f(n, m);
+    }
+
+    public int f(int n, int m) {
+        if (n == 1) {
+            return 0;
+        }
+        int x = f(n - 1, m);
+        return (m + x) % n;
+    }
+}

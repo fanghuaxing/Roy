0a4a6a4a378c803f4bf4c1f4ff5ec19a1ddb7281
roy <lixianji85@sina.com>
2022-04-13 22:17:02 +0800

    update algorithm module

--- algorithm/src/main/java/sword_offer/to75/Solution_62.java
+++ algorithm/src/main/java/sword_offer/to75/Solution_62.java
@@ -1,16 +1,16 @@
 package sword_offer.to75;
 
-
+/**
+ * 圆圈中最后剩下的数字
+ */
 public class Solution_62 {
     public int lastRemaining(int n, int m) {
         return f(n, m);
     }
-
     public int f(int n, int m) {
-        if (n == 1) {
-            return 0;
-        }
+        if (n == 1) {return 0;}
         int x = f(n - 1, m);
         return (m + x) % n;
     }
+
 }

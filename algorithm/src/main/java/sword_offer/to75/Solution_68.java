500de85d2b547823c83884bf04658928cf0e63ec
roy <lixianji85@sina.com>
2022-04-15 23:27:38 +0800

    update algorithm module

---
+++ algorithm/src/main/java/sword_offer/to75/Solution_68.java
@@ -0,0 +1,20 @@
+package sword_offer.to75;
+
+
+import sword_offer.TreeNode;
+
+public class Solution_68 {
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        TreeNode ancestor = root;
+        while (true) {
+            if (p.val < ancestor.val && q.val < ancestor.val) {
+                ancestor = ancestor.left;
+            } else if (p.val > ancestor.val && q.val > ancestor.val) {
+                ancestor = ancestor.right;
+            } else {
+                break;
+            }
+        }
+        return ancestor;
+    }
+}

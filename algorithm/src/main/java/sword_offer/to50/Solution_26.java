190edcaad3792a1da2c49d712c57bea893c56b54
roy <lixianji85@sina.com>
2022-04-19 23:17:17 +0800

    update algorithm module

---
+++ algorithm/src/main/java/sword_offer/to50/Solution_26.java
@@ -0,0 +1,21 @@
+package sword_offer.to50;
+
+import sword_offer.TreeNode;
+
+/**
+ * 输入两棵二叉树A和B，判断B是不是A的子结构。(约定空树不是任意一个树的子结构)
+ * B是A的子结构， 即 A中有出现和B相同的结构和节点值。
+ */
+public class Solution_26 {
+
+    public boolean isSubStructure(TreeNode a, TreeNode b) {
+        if ((a == null || b == null)) return false;
+        return recur(a, b) || isSubStructure(a.left, b) || isSubStructure(a.right, b);
+    }
+
+    boolean recur(TreeNode A, TreeNode B) {
+        if (B == null) return true;
+        if (A == null || A.val != B.val) return false;
+        return recur(A.left, B.left) && recur(A.right, B.right);
+    }
+}

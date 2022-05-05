cdf8a7d4ae95080f11a3b7b0ccb3591046e97578
roy <lixianji85@sina.com>
2022-05-05 23:39:20 +0800

    update algorithm module

--- algorithm/src/main/java/sword_offer/to50/Solution_34.java
+++ algorithm/src/main/java/sword_offer/to50/Solution_34.java
@@ -7,24 +7,28 @@ import java.util.Deque;
 import java.util.LinkedList;
 import java.util.List;
 
+/**
+ * 给你二叉树的根节点 root 和一个整数目标和 targetSum ，
+ * 找出所有 从根节点到叶子节点 路径总和等于给定目标和的路径。
+ */
 public class Solution_34 {
-    List<List<Integer>> ret = new LinkedList<List<Integer>>();
+    List<List<Integer>> ret = new LinkedList<>();
 
-    Deque<Integer> path = new LinkedList<Integer>();
+    Deque<Integer> path = new LinkedList<>();
 
     public List<List<Integer>> pathSum(TreeNode root, int target) {
         dfs(root, target);
         return ret;
     }
 
-    public void dfs(TreeNode root, int target) {
+    private void dfs(TreeNode root, int target) {
         if (root == null) {
             return;
         }
         path.offerLast(root.val);
         target -= root.val;
         if (root.left == null && root.right == null && target == 0) {
-            ret.add(new LinkedList<Integer>(path));
+            ret.add(new LinkedList<>(path));
         }
         dfs(root.left, target);
         dfs(root.right, target);
